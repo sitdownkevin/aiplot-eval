3ede854fe9e1ad6518e96a136db4ccadda16ef82
Ke Xu <kexu567@gmail.com>
2025-05-27 14:01:41 +0800

    Update code

--- code/config.py
+++ code/config.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+import os
+
 class Config:
-    OPENAI_BASE_URL = ""
-    OPENAI_API_KEY = ""
+    OPENAI_BASE_URL = os.environ.get("OPENAI_BASE_URL", "https://burn.hair/v1")
+    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "sk-xxx")
 
     DRAMA_AGENT_MODEL_PROVIDER = "openailike"
-    DRAMA_AGENT_MODEL_NAME = "gpt-4o"
+    DRAMA_AGENT_MODEL_NAME = os.environ.get("OPENAI_MODEL_NAME", "gpt-4o")
 
     SCRIPTWRITER_AGENT_MODEL_PROVIDER = ""
     SCRIPTWRITER_AGENT_MODEL_NAME = ""
