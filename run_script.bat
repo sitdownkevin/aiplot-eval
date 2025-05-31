f8497c1830df55f1c496c3d981c01cee6fb4bf68
fangzhanlin <934814745@qq.com>
2025-06-01 00:44:22 +0800

    reset gamelog

--- run_script.bat
+++ run_script.bat
@@ -8,8 +8,16 @@ rem 运行你的程序
 @REM uv run ./code/gai/SceneChainAndNormEnding.py
 @REM uv run ./code/gai/SceneStreamByChain.py
 @REM uv run ./code/gai/SceneInteractionAndTrigger.py
-uv run ./code/scriptwriter.py
+@REM uv run ./code/scriptwriter.py
 @REM uv run test/test_offline.py
+
+rem 循环运行程序十次
+for /l %%i in (1,1,3) do (
+    echo running %%i ...
+    uv run test/test_offline.py
+    echo  %%i running end
+    echo.
+)
 
 rem 如果需要确保窗口不要自动关闭，可以加上一句
 @REM pause
