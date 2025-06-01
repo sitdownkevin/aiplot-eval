f177c44cd7f093ce2eaca5fdd5ab3b368ce7adf0
fangzhanlin <934814745@qq.com>
2025-06-02 01:28:13 +0800

    run testoffline_sidestory 10 times

--- run_script.bat
+++ run_script.bat
@@ -8,16 +8,16 @@ rem 运行你的程序
 @REM uv run ./code/gai/SceneChainAndNormEnding.py
 @REM uv run ./code/gai/SceneStreamByChain.py
 @REM uv run ./code/gai/SceneInteractionAndTrigger.py
-uv run ./code/scriptwriter.py
+@REM uv run ./code/scriptwriter.py
 @REM uv run test/test_offline.py
 @REM uv run ./code/test_offline_sidestory.py
 rem 循环运行程序十次
-@REM for /l %%i in (1,1,1) do (
-@REM     echo running %%i ...
-@REM     uv run ./code/test_offline_sidestory.py
-@REM     echo  %%i running end
-@REM     echo.
-@REM )
+for /l %%i in (1,1,10) do (
+    echo running %%i ...
+    uv run ./code/test_offline_sidestory.py
+    echo  %%i running end
+    echo.
+)
 
 rem 如果需要确保窗口不要自动关闭，可以加上一句
 @REM pause
